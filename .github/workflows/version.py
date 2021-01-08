ebfbce38e7110d97b4817af1333c25b6817b46b7
Peter Buffon <pabuffon@gmail.com>
2021-01-08 14:32:30 -0800

    Update version.py

--- .github/workflows/version.py
+++ .github/workflows/version.py
@@ -1,4 +1,5 @@
-from sys import argv
+from sys import argv, exit
+import yaml
 
 with open(argv[1], "rb") as f:
     byte_string = f.read()
@@ -7,9 +8,20 @@ for byte in byte_string:
     text += chr(byte)
 key_string = "https://api-staging.mojang.com\x00\x00"
 version_position = text.find(key_string) + len(key_string)
+if not version_position:
+    exit("Version number not found.")
 version = ""
 for char in text[version_position:]:
     if char == "\x00":
         break
     version += char
-print(version)
+
+with open("snapcraft.yaml") as f:
+    snapcraft = yaml.safe_load(f.read())
+
+if snapcraft["version"] == version:
+    exit("Already at latest version.")
+else:
+    snapcraft["version"] = version
+    with open("snapcraft.yaml", "w") as f:
+        f.write(yaml.dump(snapcraft))
