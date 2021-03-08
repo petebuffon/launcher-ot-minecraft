28021095a08b7b85a96bdbee8a2369c53ba542a4
Peter Buffon <pabuffon@gmail.com>
2021-03-08 10:41:51 -0800

    Update version.py
    bootstrap update

--- .github/workflows/version.py
+++ .github/workflows/version.py
@@ -1,4 +1,5 @@
 from sys import argv, exit
+import requests
 import yaml
 
 with open(argv[1], "rb") as f:
@@ -6,6 +7,8 @@ with open(argv[1], "rb") as f:
 text = ""
 for byte in byte_string:
     text += chr(byte)
+    
+"""
 key_string = "https://api-staging.mojang.com\x00\x00"
 version_position = text.find(key_string) + len(key_string)
 if not version_position:
@@ -15,7 +18,18 @@ for char in text[version_position:]:
     if char == "\x00":
         break
     version += char
+"""
+
+key_string = "https://launchermeta.mojang.com/v1/products/launcher/"
+pos = text.find(key_string)
+url = ""
+while text[pos] != "\x00":
+    url += text[pos]
+    pos += 1
     
+r = requests.get(url)
+version = r.json()["launcher-bootstrap"][0]["version"]["name"]
+
 with open("snapcraft.yaml") as f:
     snapcraft = yaml.safe_load(f.read())
 
